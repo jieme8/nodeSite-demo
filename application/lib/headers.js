918ee883b7741a9936b878d908e7fc0957522385
jieme8@gmail.com <jieme8@gmail.com>
2015-12-15 10:52:50 +0800

    modified

--- application/lib/headers.js
+++ application/lib/headers.js
@@ -2,3 +2,8 @@ exports.head = function(req, res, next) {
 	res.setHeader("X-Powered-By","jie");
 	next();
 }
+
+exports.head2 = function(req, res, next) {
+	res.setHeader("head2","true");
+	next();
+}
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var index = require('../application/controllers');
+var headers     = require('../application/lib/headers');
 
 
 //路由配置
 router.get('/',index.index);								//node标准模式
 router.get('/cookie',index.cookie);							//cookie的使用
-router.get('/h',index.h);	
+router.get('/h',headers.head2,index.h);						//头信息
 
 
 module.exports = router;
